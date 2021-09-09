265ebe398eae2d3402bce5c7ffcf0f3105e8652c
banglahouse <61039594+banglahouse@users.noreply.github.com>
2021-09-09 14:11:59 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@
 from django.db.models import fields
 from django.db.models.base import Model
 from rest_framework import serializers
-from .models import stock, articles,options,investments
+from .models import stock, articles,optionsStocks,investments
 
 
 class stockApiSerializers(serializers.ModelSerializer):
